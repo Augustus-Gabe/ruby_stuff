0fbe1afabeaa6addd2bdbbabc41845ff182d0ebf
Gabriel Augusto da Silva <augustusgabe@outlook.com>
2018-10-06 20:54:40 -0300

    inserida opção de busca por termo

--- task_list.rb
+++ task_list.rb
@@ -1,3 +1,4 @@
+require 'pry'
 puts "Bem-vindo ao Task List!" 
 
 ativo = 1
@@ -9,35 +10,58 @@ def menu()
   puts "Escolha uma opção no menu: \n"
   puts '[1] Inserir uma tarefa'
   puts '[2] Ver todas as tarefas'
-  puts '[3] Encerrar programa.'
+  puts '[3] Buscar tarefas'
+  puts '[4] Encerrar programa.'
   puts
   print 'Opção escolhida: '
 
   gets.to_i
 end
 
+def busca(lista_tarefas)
+  elementos_encontrados = []
+  if lista_tarefas.length > 0
+    termo = gets.strip
+    lista_tarefas.each do |item|
+      print item.downcase.include? termo.downcase
+      if item.downcase.include? termo.downcase
+        elementos_encontrados << item
+      end
+    end
+      if elementos_encontrados.length > 0
+        puts 
+        puts "foram encontrados os elementos a seguir \n "
+        return elementos_encontrados
+      end
+      return 'não foi encontrado nenhum elemento com o termo usado.'
+  end
+  return 'não há itens na lista!'
+end
+
 while ativo == 1
  
   opcao = menu()
 
+  system('clear')
+    
   if opcao == 1
     print 'Digite sua tarefa: '
     tarefa = gets
     puts
     lista_tarefas << tarefa
-    system('clear')
     puts 'Tarefa cadastrada: ' + tarefa
   elsif opcao == 2 
     puts 
-    system('clear')
     lista_tarefas.each_with_index {|item, index|
       puts "##{index + 1} - #{item}"
     }
   elsif opcao == 3
+    puts
+    puts busca(lista_tarefas)
+  elsif opcao == 4
     ativo = 0
   else
     puts
-    system('clear')
     puts 'Opcao inválida'
   end
 end
